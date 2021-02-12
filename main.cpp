abbce1b3a3881ba0ddec185a4088f9566c806532
Will Seymour <willseymour804@gmail.com>
2021-02-12 17:26:51 -0600

    experimenting with remote push using pat

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main()
 	cout << "\t 1. Cashier Module\n\t 2. Inventory Database Module\n\t 3. Report Module\n\t 4. Exit" << endl;
 	cout << "\n\t Enter Your Choice: " << endl;
 
-	cout << "Test";
+	
 	return 0;
 
 }

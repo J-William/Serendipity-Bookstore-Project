9bd54e004b8ec3a00a3bdf008dad6331b318107e
Will Seymour <willseymour804@gmail.com>
2021-02-11 23:32:21 -0600

    Initial commit created files and version control for Serendipity Bookstore Project

---
+++ cashier.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Serendipity Book Sellers" << endl << endl;
+	cout << "Date:" << endl << endl;
+	cout << "Qty    ISBN\tTitle\t\tPrice\tTotal" << endl;
+	cout << "--------------------------------------------" << endl << endl;
+
+	return 0;
+
+
+}
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	
+
+	cout << "\t Serendipity Booksellers" << endl << endl;
+	cout << "\t 1. Cashier Module\n\t 2. Inventory Database Module\n\t 3. Report Module\n\t 4. Exit" << endl;
+	cout << "\n\t Enter Your Choice: " << endl;
+
+	return 0;
+
+}
